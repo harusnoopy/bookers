3b0526f2129e4feecd0c8aa4327975bcc9f0a34c
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-03 14:10:40 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get 'homes/index'
   get 'homes/show'
   get 'homes/edit'
-  
+
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/show'
+  get 'books/:id' => 'books/show'
   get 'books/edit'
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
