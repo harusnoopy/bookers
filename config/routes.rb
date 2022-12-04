4432e61817a9e12b4decc120184970a801c69bc8
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-12-04 16:01:09 +0000

    add validates

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get 'homes/new'
-  get 'homes/index'
-  get 'homes/show'
-  get 'homes/edit'
-
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books/show'
-  get 'books/edit'
-
+ resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
